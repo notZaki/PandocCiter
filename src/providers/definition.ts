386730637fbf6cbacb14de2182a608198148c51a
Dom <dominicd7@hotmail.com>
2021-12-12 18:53:54 +0100

    Simplified regexp for selecting citeKeys VSCode has an inbuilt function to make this a lot easier

--- src/providers/definition.ts
+++ src/providers/definition.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import { Extension } from '../extension';
-import * as fs from 'fs';
 
 export class DefinitionProvider implements vscode.DefinitionProvider {
 	extension: Extension;
@@ -10,25 +9,14 @@ export class DefinitionProvider implements vscode.DefinitionProvider {
 	}
 
 	provideDefinition(document: vscode.TextDocument, position: vscode.Position): vscode.Location | undefined {
-		// look for an @ at the start of the current word (start) and the end of the current word or line (end)
-		const startResult = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position)).match(/[\b@]\w*$/);
-		const endResult = document.getText(new vscode.Range(position, new vscode.Position(position.line, 65535))).match(/$|\s/);
-		if (startResult === null || endResult === null ||
-			startResult.index === undefined || endResult.index === undefined ||
-			startResult.index < 0 || endResult.index < 0) {
-			return undefined;
-		}
-
-		const invoker = startResult[0][0];
-		if (invoker !== '@') { return; }
-
-		const line = document.getText(new vscode.Range(
-			new vscode.Position(position.line, startResult.index + 1),
-			new vscode.Position(position.line, position.character + endResult.index)
-		)).trim()
-		const cite = this.extension.completer.citation.getEntry(line);
-		if (cite) {
-			return new vscode.Location( vscode.Uri.file(cite.file), cite.position )
+		// a cite key is an @ symbol followed by word characters (letters or numbers)
+		const keyRange = document.getWordRangeAtPosition(position, /(?<=@)\w+/);
+		if (keyRange){
+			const citeKey = document.getText(keyRange);
+			const cite = this.extension.completer.citation.getEntry(citeKey);
+			if (cite) {
+				return new vscode.Location(vscode.Uri.file(cite.file), cite.position);
+			}
 		}
 	}
 }
--- src/providers/hover.ts
+++ src/providers/hover.ts
@@ -9,33 +9,18 @@ export class HoverProvider implements vscode.HoverProvider {
 	}
 
 	public async provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.Hover | undefined> {
-
-		// look for an @ at the start of the current word (start) and the end of the current word or line (end)
-		const startResult = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position)).match(/[\b@]\w*$/);
-		const endResult = document.getText(new vscode.Range(position, new vscode.Position(position.line, 65535))).match(/$|\s/);
-		if (startResult === null || endResult === null ||
-			startResult.index === undefined || endResult.index === undefined ||
-			startResult.index < 0 || endResult.index < 0) {
-			return undefined;
-		}
-
-		const invoker = startResult[0][0];
-		if (invoker !== '@') { return; }
-
-		const line = document.getText(new vscode.Range(
-			new vscode.Position(position.line, startResult.index + 1),
-			new vscode.Position(position.line, position.character + endResult.index)
-		)).trim()
-		const cite = this.extension.completer.citation.getEntry(line);
-
-		if (cite) {
-			let md = cite.documentation || cite.detail;
-			md = md.replace(/\n/g, '  \n'); // need double space then a newline to actually get a newline in markdown
-			if (md) {
-				return new vscode.Hover(md)
+		// a cite key is an @ symbol followed by word characters (letters or numbers)
+		const keyRange = document.getWordRangeAtPosition(position, /(?<=@)\w+/);
+		if (keyRange){
+			const citeKey = document.getText(keyRange);
+			const cite = this.extension.completer.citation.getEntry(citeKey);
+			if (cite) {
+				let hoverMarkdownText = cite.documentation || cite.detail;
+				hoverMarkdownText = hoverMarkdownText.replace(/\n/g, '  \n'); // need double space then a newline to actually get a newline in markdown
+				if (hoverMarkdownText) {
+					return new vscode.Hover(hoverMarkdownText)
+				}
 			}
 		}
-
-		return undefined;
 	}
 }
